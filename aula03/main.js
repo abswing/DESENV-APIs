3b6d9c56098a3e63cf2da7417f93ddbe0d7e2581
Bernardo Jesus <antoniobernardo132005@gmail.com>
2025-03-25 20:22:56 -0300

    add main novamente

--- aula03/main.js
+++ aula03/main.js
@@ -1,13 +1,12 @@
-const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('BERNARDO TESTE');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
 
-console.log("ola bernardo 01");
+
+const PI = 3.14;
+let numero = 10;
+// const PI = 3.15
+numero = 11;
+
+console.log("PI", PI);
+console.log("Numero", numero);
+
+console.log("igual: ", numero == "11");
+console.log("igual; ", numero === "11")
