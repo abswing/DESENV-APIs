1314f306bf915ee9d1e7327adb12039370ecb2d6
Bernardo Jesus <antoniobernardo132005@gmail.com>
2025-03-25 20:22:13 -0300

    add main novo

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+const PI = 3.14;
+let numero = 10;
+// const PI = 3.15
+numero = 11;
+
+console.log("PI", PI);
+console.log("Numero", numero);
+
+console.log("igual: ", numero == "11");
+console.log("igual; ", numero === "11")
