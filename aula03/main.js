9ccd5f4617535eee9478fd7d3f83701fb5ad0b6f
Bernardo Jesus <antoniobernardo132005@gmail.com>
2025-03-25 21:42:25 -0300

    add mais algumas coisa na aula

--- aula03/main.js
+++ aula03/main.js
@@ -9,4 +9,29 @@ console.log("PI", PI);
 console.log("Numero", numero);
 
 console.log("igual: ", numero == "11");
-console.log("igual; ", numero === "11")
+console.log("igual; ", numero === "11");
+
+
+const estados = (numero > 10) 
+    ? "maior"
+    : "menor ou igual";
+console.log("estado: ",estados);
+
+let x = 20;
+let y = 30;
+
+if(x > y){
+    console.log("condicao: ", "x > y");
+} else if(x < y ){
+    console.log("condicao: ", "x < y" );
+} else{
+    console.log("condicao: ", "x = y");
+}
+
+for (let i=0; i<5; i++) {
+    console.log("i: ", i);
+}
+let marcasCarros = ["GM","FIAT", "RENAUL"];
+for(let indice in marcasCarros){
+    console.log("indice: ", indice+'-',marcasCarros );
+}
